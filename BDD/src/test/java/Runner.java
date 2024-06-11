74dcfb986829a208c44c7c027de0a22ad3cf79b1
Alexandre Sampaio <alexandre.rs.ale17@gmail.com>
2024-06-11 18:24:06 -0300

    Add files via upload

---
+++ BDD/src/test/java/Runner.java
@@ -0,0 +1,9 @@
+// Classe principal para iniciar a execução dos cenários de teste com o Cucumber
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class Runner {
+
+}
