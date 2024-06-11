2d4e3003ee29ecdfad82e47c4e27d2801c94b3a9
Alexandre Sampaio <alexandre.rs.ale17@gmail.com>
2024-06-11 18:27:41 -0300

    Delete BDD directory

--- BDD/src/test/java/Runner.java
+++
@@ -1,9 +0,0 @@
-// Classe principal para iniciar a execução dos cenários de teste com o Cucumber
-
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-public class Runner {
-
-}
